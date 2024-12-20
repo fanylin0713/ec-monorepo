06651c4aab04a434934470d8370874f0fe9688ca
Fany <fanylin@japan-import.com.tw>
2024-12-20 10:31:32 +0800

    feat: add local libraries

--- apps/ec-store-e2e/eslint.config.js
+++ apps/ec-store-e2e/eslint.config.js
@@ -1,7 +1,10 @@
+const baseConfig = require('../../eslint.base.config.js');
 const playwright = require('eslint-plugin-playwright');
 const baseConfig = require('../../eslint.config.js');
 
 module.exports = [
+  ...baseConfig,
+
   playwright.configs['flat/recommended'],
 
   ...baseConfig,
---
+++ lib/products/src/lib/products.tsx
@@ -0,0 +1,14 @@
+import styled from 'tailwind';
+
+const StyledProducts = styled.div`
+  color: pink;
+`;
+export function Products() {
+  return (
+    <StyledProducts>
+      <h1>Welcome to Products!</h1>
+    </StyledProducts>
+  );
+}
+
+export default Products;
